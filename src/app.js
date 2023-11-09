e8cb4b9aeaa65e214952e9c1f2895db38702a3c7
Davis Keeney <142953893+wdavisk123@users.noreply.github.com>
2023-11-09 02:10:50 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,70 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "my", "your", "his", "her"];
+  let adjectives = [
+    "great",
+    "big",
+    "orange",
+    "spooky",
+    "terrific",
+    "wonderful",
+    "amazing",
+    "painted",
+    "pretty",
+    "excellent",
+    "tasty",
+    "humble",
+    "gentle",
+    "quiet",
+    "loud"
+  ];
+  let nouns = [
+    "jogger",
+    "racoon",
+    "saloon",
+    "palace",
+    "cafe",
+    "farm",
+    "hamster",
+    "deer",
+    "smokehouse",
+    "bar",
+    "music hall",
+    "beauty shop",
+    "castle",
+    "hotel",
+    "diner",
+    "theater"
+  ];
+  let domains = [];
+  let domCom = [".com", ".net", ".gov"];
+
+  const generateDomains = () => {
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjectives.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          for (let l = 0; l < domCom.length; l++) {
+            let domain = pronouns[i] + adjectives[j] + nouns[k] + domCom[l];
+            domains.push(domain);
+          }
+        }
+      }
+    }
+    displayDomains();
+  };
+
+  const displayDomains = () => {
+    const domainContainer = document.getElementById("domainContainer");
+    domainContainer.innerHTML = "";
+
+    domains.forEach(domain => {
+      const domainElement = document.createElement("p");
+      domainElement.textContent = domain;
+      domainContainer.appendChild(domainElement);
+    });
+  };
+
+  const generateButton = document.getElementById("generateButton");
+  generateButton.addEventListener("click", generateDomains);
 };
